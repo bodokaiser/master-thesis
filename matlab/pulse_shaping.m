598b1ddbf0120b6e9d9041881721166bbadc632c
Bodo Kaiser <i@bodokaiser.io>
2021-07-19 14:28:30 +0200

    added, updated figurs for cv-qkd chapters

--- matlab/pulse_shaping.m
+++ matlab/pulse_shaping.m
@@ -1,7 +1,9 @@
 % number of symbols
-nsym = 128;
-% samples per symbol (upsampling factor)
-sps = 4;
+nsym = 20*128;
+% upsampling RRC
+uf_rrc = 2;
+% upsampling LP
+uf_lp = 4;
 
 % complex random symbols
 x1 = 2*rand(nsym, 1) - 1;
@@ -9,20 +11,20 @@ p1 = 2*rand(length(x1), 1) - 1;
 alpha1 = x1 + p1*1i;
 
 % upsampling
-alpha2 = upsample(alpha1, sps);
+alpha2 = upsample(alpha1, uf_rrc);
 
 % symbol time
 t1 = (0:(length(alpha1) - 1)).';
-t2 = (0:(length(alpha2) - 1)).'/sps;
+t2 = (0:(length(alpha2) - 1)).'/uf_rrc;
 
 % rrc filtering
-rrc = rcosdesign(0.8, 16, sps, 'sqrt');
-alpha3 = conv(alpha2, rrc / rrc(8*uf+1), 'same');
+rrc = rcosdesign(0.8, 16, uf_rrc, 'sqrt');
+alpha3 = conv(alpha2, rrc / rrc(8*uf_rrc+1), 'same');
 
 % lowpass filtering
 [b, a] = butter(7, 0.08);
-alpha4 = filtfilt(b, a, upsample(alpha3, 10));
-t4 = (0:(length(alpha4) - 1)).'/(sps*10);
+alpha4 = filtfilt(b, a, upsample(alpha3, uf_lp));
+t4 = (0:(length(alpha4) - 1)).'/(uf_rrc*uf_lp);
 
 figure
 tl = tiledlayout(4, 1);
@@ -58,17 +60,23 @@ xlim(symlims)
 title('Lowpass');
 hold off
 
-[pxx1,f1] = pwelch(alpha1, [], [], 'centered');
-[pxx2,f2] = pwelch(alpha2, [], [], 'centered');
-[pxx3,f3] = pwelch(alpha3, [], [], 'centered');
-[pxx4,f4] = pwelch(alpha4, [], [], 'centered');
+%[pxx1,f1] = pwelch(alpha1, [], [], [], 1, 'centered');
+%[pxx2,f2] = pwelch(alpha2, [], [], [], uf_rrc, 'centered');
+%[pxx3,f3] = pwelch(alpha3, [], [], [], uf_rrc, 'centered');
+%[pxx4,f4] = pwelch(alpha4, [], [], [], uf_rrc * uf_lp, 'centered');
 
 figure
-plot(fftshift(f1), fftshift(pxx1) / sum(pxx1))
+pwelch(alpha4, [], [], [], 1, 'centered');
+%plot(f4, 10*log10(pxx4./max(pxx4)))
 hold on
-plot(fftshift(f2), fftshift(pxx2) / sum(pxx2))
-plot(fftshift(f3), fftshift(pxx3) / sum(pxx3))
-plot(fftshift(f4), fftshift(pxx4) / sum(pxx4))
-legend('Symbols', 'Upsampling', 'Root-raised-cosine', 'Lowpass')
+pwelch(alpha3, [], [], [], 1/uf_lp, 'centered');
+pwelch(alpha2, [], [], [], 1/uf_lp, 'centered');
+pwelch(alpha1, [], [], [], 1/(uf_lp*uf_rrc), 'centered');
+%plot(f3, 10*log10(pxx3./max(pxx3)))
+%plot(f2, 10*log10(pxx2./max(pxx2)))
+%plot(f1, 10*log10(pxx1./max(pxx1)))
+%xlim([-1,1])
+%plot(fftshift(f4), 10*log10(fftshift(pxx4) / sum(pxx4)))
+%legend('Symbols', 'Upsampling', 'Root-raised-cosine')%, 'Lowpass')
 grid on
 hold off
