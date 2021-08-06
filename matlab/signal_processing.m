d8e52804d7aad56219d5c4cd6d358b3d99fb0440
Bodo Kaiser <i@bodokaiser.io>
2021-08-06 15:51:21 +0200

    rewrote matlab script, updated transmitter pulse-shaping plots

--- matlab/signal_processing.m
+++ matlab/signal_processing.m
@@ -6,80 +6,77 @@ uf_ps = 2;
 % upsampling factor for low-pass filter
 uf_lp = 4;
 
-% unit response
-unit_sym = unitsym(num_sym, 16);
-[unit_ps_up, unit_ps_out] = pulse_shape_filter(unit_sym, uf_ps);
-[unit_lp_up, unit_lp_out] = low_pass_filter(unit_ps_out, uf_lp);
-[unit_lp_up2, unit_lp_out2] = low_pass_filter(unit_sym, uf_lp);
-%plot_time(num_sym, [10, 20], unit_sym, unit_ps_up, unit_ps_out, unit_lp_out)
-%plot_freq(length(lp_out), [-0.4, +0.4],  unit_sym, unit_ps_up, unit_ps_out, unit_lp_up, unit_lp_out, unit_lp_out2)
-%write_time(num_sym, unit_sym, '../data/pulse-shaping/unit-time-sym.csv');
-%write_time(num_sym, unit_ps_up, '../data/pulse-shaping/unit-time-psup.csv');
-%write_time(num_sym, unit_ps_out, '../data/pulse-shaping/unit-time-psout.csv');
-%write_time(num_sym, unit_lp_out / max(unit_lp_out), '../data/pulse-shaping/unit-time-lpout.csv');
-%write_freq(num_sym, unit_sym, '../data/pulse-shaping/unit-freq-sym.csv');
-%write_freq(num_sym, unit_ps_up, '../data/pulse-shaping/unit-freq-psup.csv');
-%write_freq(num_sym, unit_ps_out, '../data/pulse-shaping/unit-freq-psout.csv');
-%write_freq(num_sym, unit_lp_up, '../data/pulse-shaping/unit-freq-lpup.csv');
-%write_freq(num_sym, unit_lp_out, '../data/pulse-shaping/unit-freq-lpout.csv');
-%write_freq(num_sym, unit_lp_out2, '../data/pulse-shaping/unit-freq-lpout2.csv');
+% RRC filter
+rrc_b = rcosdesign(0.25, 16, uf_ps, 'sqrt');
+% LP filter
+[lp_b, lp_a] = butter(7, 0.2);
 
-% filter
-rrc = rcosdesign(0.25, 16, uf_ps, 'sqrt');
+% unit symbol
+unit_tx_sym = zeros(num_sym, 2) * [1; 1i];
+unit_tx_sym(16) = 1;
+unit_tx_up = upsample(unit_tx_sym, uf_ps);
+unit_tx_rrc = conv(unit_tx_up, rrc_b, 'same');
+unit_tx_dac = upsample(unit_tx_rrc, uf_ps);
+unit_tx_lp = filtfilt(lp_b, lp_a, unit_tx_dac);
 
 % random symbols
-rnd_sym = randsym(num_sym);
-rnd_ps_up = upsample(rnd_sym, uf_ps);
-rnd_ps_out = conv(rnd_ps_up, rrc, 'same');
-[rnd_lp_up, rnd_lp_out] = low_pass_filter(rnd_ps_out, uf_lp);
-%plot_time(num_sym, [10, 20], rnd_sym, rnd_ps_up, rnd_ps_out, rnd_lp_out)
-%plot_freq(length(rnd_lp_out), [-0.4, +0.4],  rnd_sym, rnd_ps_up, rnd_ps_out, rnd_lp_up, rnd_lp_out)
+rand_tx_sym = (2*rand(num_sym, 2) - 1) * [1; 1i];
+rand_tx_up = upsample(rand_tx_sym, uf_ps);
+rand_tx_rrc = conv(rand_tx_up, rrc, 'same');
+rand_tx_dac = upsample(rand_tx_rrc, uf_lp);
+rand_tx_lp = filtfilt(lp_b, lp_a, rand_tx_dac);
+rand_rx_det = rand_tx_lp .* exp(2*pi*1i*0.8 * (0:(length(rand_tx_lp) - 1)).' / (length(rand_tx_lp) / num_sym));
+rand_rx_dwnc = rand_tx_lp;
+rand_rx_adc = downsample(rand_rx_dwnc, uf_lp);
+rand_rx_rrc = conv(rand_rx_adc, rrc, 'same');
+rand_rx_dwn = downsample(rand_rx_rrc, uf_ps);
 
-time = (0:(length(rnd_lp_out) - 1)).' / (length(rnd_lp_out) / length(rnd_sym));
+% visualize
+%plot_time(num_sym, [10, 20], unit_tx_sym, unit_tx_up, unit_tx_rrc, unit_tx_lp)
+%plot_freq(length(unit_tx_lp), [-0.4, +0.4],  unit_tx_sym, unit_tx_up, unit_tx_rrc, unit_tx_dac, unit_tx_lp)
+%plot_time(num_sym, [10, 20], rand_tx_sym, rand_tx_up, rand_tx_rrc, rand_tx_lp, rand_rx_det, rand_rx_adc, rand_rx_rrc, rand_rx_dwn)
+%plot_freq(length(rand_tx_lp), [-0.4, +0.4],  rand_tx_sym, rand_tx_up, rand_tx_rrc, rand_tx_dac, rand_tx_lp, rand_rx_det, rand_rx_dwnc, rand_rx_adc, rand_rx_rrc, rand_rx_dwn)
 
-rnd_rx_ini = rnd_lp_out .* exp(2*pi*1i*0.8 * time);
-rnd_rx_dwc = rnd_rx_ini .* exp(-2*pi*1i*0.8 * time);
-rnd_rx_ctr = downsample(rnd_rx_dwc, uf_lp);
-rnd_rx_rrc = conv(rnd_rx_ctr, rrc, 'same');
-rnd_rx_dwn = downsample(rnd_rx_rrc, uf_ps);
-plot_time(num_sym, [10, 20], rnd_rx_upc, rnd_rx_ctr, rnd_rx_rrc, rnd_rx_dwn, rnd_sym)
-plot_freq(length(rnd_lp_out), [-0.4, +0.4], rnd_rx_ini, rnd_rx_dwc, rnd_rx_ctr, rnd_rx_rrc, rnd_rx_dwn)
-%write_time(num_sym, real(rnd_sym), '../data/pulse-shaping/rand-time-sym-real.csv');
-%write_time(num_sym, imag(rnd_sym), '../data/pulse-shaping/rand-time-sym-imag.csv');
-%write_time(num_sym, real(rnd_ps_up), '../data/pulse-shaping/rand-time-psup-real.csv');
-%write_time(num_sym, imag(rnd_ps_up), '../data/pulse-shaping/rand-time-psup-imag.csv');
-%write_time(num_sym, real(rnd_ps_out), '../data/pulse-shaping/rand-time-psout-real.csv');
-%write_time(num_sym, imag(rnd_ps_out), '../data/pulse-shaping/rand-time-psout-imag.csv');
-%write_time(num_sym, 4*real(rnd_lp_out), '../data/pulse-shaping/rand-time-lpout-real.csv');
-%write_time(num_sym, 4*imag(rnd_lp_out), '../data/pulse-shaping/rand-time-lpout-imag.csv');
-%write_freq(num_sym, rnd_sym, '../data/pulse-shaping/rand-freq-sym.csv');
-%write_freq(num_sym, rnd_ps_up, '../data/pulse-shaping/rand-freq-psup.csv');
-%write_freq(num_sym, rnd_ps_out, '../data/pulse-shaping/rand-freq-psout.csv');
-%write_freq(num_sym, rnd_lp_up, '../data/pulse-shaping/rand-freq-lpup.csv');
-%write_freq(num_sym, rnd_lp_out, '../data/pulse-shaping/rand-freq-lpout.csv');
+datadir = '../data/signal-processing';
 
+% export csv
+write_time(num_sym, unit_tx_sym, fullfile(datadir, 'unit-time-tx-sym.csv'));
+write_time(num_sym, unit_tx_up, fullfile(datadir, 'unit-time-tx-up.csv'));
+write_time(num_sym, unit_tx_rrc, fullfile(datadir, 'unit-time-tx-rrc.csv'));
+write_time(num_sym, unit_tx_lp, fullfile(datadir, 'unit-time-tx-lp.csv'));
+write_freq(num_sym, unit_tx_sym, fullfile(datadir, 'unit-freq-tx-sym.csv'));
+write_freq(num_sym, unit_tx_up, fullfile(datadir, 'unit-freq-tx-up.csv'));
+write_freq(num_sym, unit_tx_rrc, fullfile(datadir, 'unit-freq-tx-rrc.csv'));
+write_freq(num_sym, unit_tx_dac, fullfile(datadir, 'unit-freq-tx-dac.csv'));
+write_freq(num_sym, unit_tx_lp, fullfile(datadir, 'unit-freq-tx-lp.csv'));
+write_time(num_sym, real(rand_tx_sym), fullfile(datadir, 'rand-time-tx-sym-real.csv'));
+write_time(num_sym, imag(rand_tx_sym), fullfile(datadir, 'rand-time-tx-sym-imag.csv'));
+write_time(num_sym, real(rand_tx_up), fullfile(datadir, 'rand-time-tx-up-real.csv'));
+write_time(num_sym, imag(rand_tx_up), fullfile(datadir, 'rand-time-tx-up-imag.csv'));
+write_time(num_sym, real(rand_tx_rrc), fullfile(datadir, 'rand-time-tx-rrc-real.csv'));
+write_time(num_sym, imag(rand_tx_rrc), fullfile(datadir, 'rand-time-tx-rrc-imag.csv'));
+write_time(num_sym, real(rand_tx_dac), fullfile(datadir, 'rand-time-tx-dac-real.csv'));
+write_time(num_sym, imag(rand_tx_dac), fullfile(datadir, 'rand-time-tx-dac-imag.csv'));
+write_time(num_sym, real(rand_tx_lp), fullfile(datadir, 'rand-time-tx-lp-real.csv'));
+write_time(num_sym, imag(rand_tx_lp), fullfile(datadir, 'rand-time-tx-lp-imag.csv'));
+write_time(num_sym, real(rand_rx_det), fullfile(datadir, 'rand-time-rx-det-real.csv'));
+write_time(num_sym, imag(rand_rx_det), fullfile(datadir, 'rand-time-rx-det-imag.csv'));
+write_time(num_sym, real(rand_rx_adc), fullfile(datadir, 'rand-time-rx-adc-real.csv'));
+write_time(num_sym, imag(rand_rx_adc), fullfile(datadir, 'rand-time-rx-adc-imag.csv'));
+write_time(num_sym, real(rand_rx_rrc), fullfile(datadir, 'rand-time-rx-rrc-real.csv'));
+write_time(num_sym, imag(rand_rx_rrc), fullfile(datadir, 'rand-time-rx-rrc-imag.csv'));
+write_time(num_sym, real(rand_rx_dwn), fullfile(datadir, 'rand-time-rx-dwn-real.csv'));
+write_time(num_sym, imag(rand_rx_dwn), fullfile(datadir, 'rand-time-rx-dwn-imag.csv'));
+write_freq(num_sym, rand_tx_sym, fullfile(datadir, 'rand-freq-tx-sym.csv'));
+write_freq(num_sym, rand_tx_up, fullfile(datadir, 'rand-freq-tx-up.csv'));
+write_freq(num_sym, rand_tx_rrc, fullfile(datadir, 'rand-freq-tx-rrc.csv'));
+write_freq(num_sym, rand_tx_dac, fullfile(datadir, 'rand-freq-tx-dac.csv'));
+write_freq(num_sym, rand_tx_lp, fullfile(datadir, 'rand-freq-tx-lp.csv'));
+write_freq(num_sym, rand_rx_det, fullfile(datadir, 'rand-freq-rx-det.csv'));
+write_freq(num_sym, rand_rx_dwnc, fullfile(datadir, 'rand-freq-rx-dwnc.csv'));
+write_freq(num_sym, rand_rx_rrc, fullfile(datadir, 'rand-freq-rx-rrc.csv'));
+write_freq(num_sym, rand_rx_dwn, fullfile(datadir, 'rand-freq-rx-dwn.csv'));
 
-function x = unitsym(n, i)
-    x = zeros(n, 2) * [1; 1i];
-    x(i) = 1;
-end
-
-function x = randsym(n)
-    x = (2*rand(n, 2) - 1) * [1; 1i];
-end
-
-function [up, out] = pulse_shape_filter(x, uf)
-    b = rcosdesign(0.25, 16, uf, 'sqrt');
-    %b = b / b(8*uf + 1);
-    up = upsample(x, uf);
-    out = conv(up, b, 'same');
-end
-
-function [up, out] = low_pass_filter(x, uf)
-    [b, a] = butter(7, 0.2);
-    up = upsample(x, uf);
-    out = filtfilt(b, a, up);
-end
 
 function [] = plot_time(n, nlim, varargin)
     figure
@@ -112,7 +109,6 @@ function [] = plot_freq(n, flim, varargin)
         nexttile
         plot(f, 10*log10(pxx))
         xlim(flim)
-        ylim([-70,0])
     end
     
     xlabel(tl, 'Frequency')
